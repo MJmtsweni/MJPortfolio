3ae2cd0a6394dd4795f84895e95f5905befcc1d5
mjmtsweni <tmtsweni@hotmail.com>
2020-03-03 13:09:41 +0200

    Sliders added

--- dist/js/mian.js
+++ dist/js/mian.js
@@ -31,3 +31,49 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Slider
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+//clear all slides
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+//slide left
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+//slide right
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+//click left
+arrowLeft.addEventListener("click", function() {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+//click left
+arrowRight.addEventListener("click", function() {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
